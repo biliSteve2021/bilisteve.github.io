8bff1a8de90ee9168ff77379bc563ca6c75da186
biliSteve2021 <144593683+biliSteve2021@users.noreply.github.com>
2025-05-17 19:05:41 +0800

    Add files via upload

---
+++ js/manifest.js
@@ -0,0 +1,16 @@
+(function () {  
+
+_mrmcp = (typeof _mrmcp == 'undefined') ? {} : _mrmcp;
+_mrmcp['campaign_id'] = 'none';
+_mrmcp['owner_id'] = '682853268a3b2002d10157cb';
+_mrmcp['creative_id'] = '682864ebf66400c1a40de39d';
+_mrmcp['ga_url'] = _mrmcp['ga_url'] || (('https:' == document.location.protocol ? 'https://' : 'http://') + 'card.mugeda.com/weixin/card/ga.js');
+_mrmcp['width'] = _mrmcp['width'] || 1280;
+_mrmcp['height'] = _mrmcp['height'] || 720;
+_mrmcp['type'] = 'smart';
+_mrmcp['title'] = 'webpage';
+_mrmcp['build_number'] = parseInt('412');
+_mrmcp['publish_time'] = 'Sat May 17 2025 18:52:36 GMT+0800 (China Standard Time)';
+_mrmcp['track_bot'] = 'http://cdn.mugeda.com/media/pages/track/track_20131030.html';
+_mrmcp['data_server'] = "https://weika.mugeda.com";
+})();
